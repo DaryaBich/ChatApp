27b89483200f6c2f0322462e9ff2bccae1d9a9c1
Darya <daryabich98@gmail.com>
2020-05-29 14:15:00 +0300

    fix insert message

--- src/main/java/example/web/controllers/SendMessageController.java
+++ src/main/java/example/web/controllers/SendMessageController.java
@@ -3,15 +3,12 @@ package example.web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.*;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/user/chatapp")
@@ -27,31 +24,26 @@ public class SendMessageController {
         long userId = (long) session.getAttribute("userId");
         long chatId = (long) session.getAttribute("chatId");
         Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("select max(id) from message");
-        ResultSet maxMessageId = preparedStatement.executeQuery();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO MESSAGE(sentByUserWithId, " +
+                "text, sendingDate) VALUES(?, ?, NOW())");
+        preparedStatement.setLong(1, userId);
+        preparedStatement.setString(2, text);
+        preparedStatement.executeUpdate();
+        preparedStatement = connection.prepareStatement("select max(id) from message where sentByUserWithId = ? " +
+                "and text = ?");
+        preparedStatement.setLong(1, userId);
+        preparedStatement.setString(2, text);
+        ResultSet lastMessageId = preparedStatement.executeQuery();
         long messageId;
-        if (maxMessageId.next()) {
-            messageId = maxMessageId.getLong("max(id)") + 1;
+        if (lastMessageId.next()) {
+            messageId = lastMessageId.getLong("max(id)");
         } else {
             messageId = 1;
         }
-        preparedStatement = connection.prepareStatement("select max(id) from MessageProcessing");
-        ResultSet maxMessageProcessingId = preparedStatement.executeQuery();
-        long messageProcessingId;
-        if (maxMessageProcessingId.next()) {
-            messageProcessingId = maxMessageProcessingId.getLong("max(id)") + 1;
-        } else {
-            messageProcessingId = 1;
-        }
-        preparedStatement = connection.prepareStatement("INSERT INTO MESSAGE VALUES( ?, ?, '?', NOW())");
-        preparedStatement.setLong(1, messageId);
-        preparedStatement.setLong(2, userId);
-        preparedStatement.setString(3, text);
-        preparedStatement.executeUpdate();
-        preparedStatement = connection.prepareStatement("INSERT INTO MESSAGEPROCESSING VALUES(?, ?, ? )");
-        preparedStatement.setLong(1, messageProcessingId);
-        preparedStatement.setLong(2, chatId);
-        preparedStatement.setLong(3, messageId);
+        preparedStatement = connection.prepareStatement("INSERT INTO MESSAGEPROCESSING(idchat, idmessage) " +
+                "VALUES(?, ?)");
+        preparedStatement.setLong(1, chatId);
+        preparedStatement.setLong(2, messageId);
         preparedStatement.executeUpdate();
         preparedStatement.close();
         response.sendRedirect("/user/chatapp/adaptchat");
